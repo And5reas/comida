41ebd8c486aa62c771644f088e117e9e4a273b64
And5reas <And5reas.porcel@hotmail.com>
2025-01-20 11:57:12 -0300

    On main_drawer replaced pushNamed to pushReplacementNamed

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:comida/components/main_drawer.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -15,6 +16,7 @@ class SettingsScreen extends StatelessWidget {
           ),
         ),
       ),
+      drawer: MainDrawer(),
       body: Center(
         child: Text('Configurações'),
       ),
