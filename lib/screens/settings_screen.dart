d8a7c3fc09c8f77dc6266ef35f13ddbcc4906dc5
And5reas <And5reas.porcel@hotmail.com>
2025-01-20 12:21:50 -0300

    Created setting model

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,25 +1,88 @@
+import 'package:comida/models/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:comida/components/main_drawer.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  var settings = Settings();
+
+  Widget _createSwitch(
+    String title,
+    String subtitle,
+    bool value,
+    Function(bool) onChanged,
+  ) {
+    return SwitchListTile.adaptive(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      value: value,
+      onChanged: onChanged,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Configurações',
-          style: TextStyle(
-            color: Theme.of(context).colorScheme.onPrimary,
-            fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          title: Text(
+            'Configurações',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.onPrimary,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-      ),
-      drawer: MainDrawer(),
-      body: Center(
-        child: Text('Configurações'),
-      ),
-    );
+        drawer: MainDrawer(),
+        body: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                'Filtros',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  _createSwitch(
+                      'Sem Glúten',
+                      'Só exibe pratos sem glúten',
+                      settings.isGlutenFree,
+                      (value) => setState(() {
+                            settings.isGlutenFree = value;
+                          })),
+                  _createSwitch(
+                      'Sem Lactose',
+                      'Só exibe pratos sem lactose',
+                      settings.isLactoseFree,
+                      (value) => setState(() {
+                            settings.isLactoseFree = value;
+                          })),
+                  _createSwitch(
+                      'Vegana',
+                      'Só exibe pratos veganas',
+                      settings.isVegan,
+                      (value) => setState(() {
+                            settings.isVegan = value;
+                          })),
+                  _createSwitch(
+                      'Vegetariana',
+                      'Só exibe pratos Vegetarianos',
+                      settings.isVegetarian,
+                      (value) => setState(() {
+                            settings.isVegetarian = value;
+                          })),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
